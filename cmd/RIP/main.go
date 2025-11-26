8c707731bccaa7903ea565160276f37d901239ec
Zagid <osmanovzagid2005@gmail.com>
2025-11-26 16:55:02 +0300

    CORS

--- cmd/RIP/main.go
+++ cmd/RIP/main.go
@@ -9,9 +9,10 @@ import (
 	"RIP/internal/pkg"
 	"context"
 	"fmt"
-
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
+	"time"
 )
 
 // @title           API для системы CoolingSystems
@@ -27,6 +28,15 @@ import (
 
 func main() {
 	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+    AllowOrigins:     []string{"http://localhost:3000"},
+    AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+    AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+    AllowCredentials: true,
+    MaxAge:           12 * time.Hour,
+  }))
+
 	conf, err := config.NewConfig()
 	if err != nil {
 		logrus.Fatalf("error loading config: %v", err)
--- go.mod
+++ go.mod
@@ -66,6 +66,7 @@ require (
 	github.com/cloudwego/base64x v0.1.6 // indirect
 	github.com/cloudwego/iasm v0.2.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.10 // indirect
+	github.com/gin-contrib/cors v1.7.6
 	github.com/gin-contrib/sse v1.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
